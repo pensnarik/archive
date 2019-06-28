805b57ffe20c240320de2dada3ac0a1c625fa842
Andrei Zhidenkov <pensnarik@gmail.com>
2019-06-28 17:34:46 +0700

    Encrypt files before backup, refresh Google API access token

--- archive/backup/google.py
+++ archive/backup/google.py
@@ -1,6 +1,7 @@
 import json
 import os.path
 import requests
+import subprocess
 
 from archive.backup import ArchiveBackup
 from archive.file_meta import get_file_hash
@@ -11,28 +12,70 @@ SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly',
 
 class ArchiveBackupGoogle(ArchiveBackup):
 
+    credentials = {}
+
     def __init__(self):
-        pass
+        with open('credentials.json') as cf:
+            self.credentials = json.load(cf)
+
+    def encrypt(self, filename, hash):
+        output_filename = "./tmp-unarchive/%s.gpg" % hash
+        subprocess.run('gpg --encrypt --recipient "Andrei Zhidenkov" --output "%s" "%s"' % \
+                       (output_filename, filename,), shell=True, check=True)
+        return output_filename
+
+    def get_headers(self):
+        return {'Authorization': 'Bearer %s' % self.credentials['access_token']}
+
+    def refresh_token(self):
+        print("Refreshing token")
+        payload = {'client_id': self.credentials['client_id'],
+                   'client_secret': self.credentials['client_secret'],
+                   'refresh_token': os.environ['ARCHIVE_GDRIVE_REFRESH_TOKEN'],
+                   'grant_type': 'refresh_token'}
+
+        r = requests.post("https://www.googleapis.com/oauth2/v4/token", data=payload)
+
+        if 'access_token' in r.json():
+            self.credentials['access_token'] = r.json()['access_token']
+            with open('credentials.json', 'wt') as f:
+                f.write(json.dumps(self.credentials))
+        else:
+            raise Exception('Could not refresh access token')
+
+        return True
 
     def backup(self, filename):
         file_hash = get_file_hash(filename)
-        headers = {'Authorization': 'Bearer %s' % os.environ['ARCHIVE_GDRIVE_TOKEN']}
+        encrypted_filename = self.encrypt(filename, file_hash)
 
         para = {
-            'name': '%s.jpg' % file_hash,
+            'name': '%s.gpg' % file_hash,
             'hash': file_hash,
             # TODO: Should be configurable
             'parents': ['1YhyEfYlfYhsSlQ37zcA5o__CJ-NHYGyd']
         }
         files = {
             'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'),
-            'file': open(filename, "rb")
+            'file': open(encrypted_filename, "rb")
         }
 
-        r = requests.post("https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
-                          headers=headers,
-                          files=files)
+        try_googleapi_request = True
 
+        while try_googleapi_request:
+            r = requests.post("https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+                              headers=self.get_headers(),
+                              files=files)
+
+            if r.status_code == 401:
+                if self.refresh_token():
+                    try_googleapi_request = True
+                else:
+                    raise Exception('Could not update API token')
+            else:
+                try_googleapi_request = False
+
+        os.remove(encrypted_filename)
         backup(file_hash, 'google', r.json().get('id'))
 
     def list(self):
